b002ce4151411943b69157bbb206d581f5ef1862
Breno França <franciscobreno.si@gmail.com>
2019-01-28 21:10:40 -0300

    primeiro_commit: criacao da aplicacao

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
+
+import store from '~/store';
+import Routes from './routes';
+
+const App = () => (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+);
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import api from '~/services/api';
+
+import { colors } from '~/styles';
+
+import { Text } from 'react-native';
+
+import styles from './styles';
+
+class Home extends Component {
+  static propTypes = {};
+
+  state = {};
+
+  componentDidMount() {}
+
+  render() {
+    return <Text>sadasd</Text>;
+  }
+}
+
+export default Home;
